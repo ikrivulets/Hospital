3b72d70d80bc0027ba35222611c885eacbb8b568
matveinazaruk <matvei.nazaruk@gmail.com>
2014-01-05 02:33:11 +0300

    mail

--- Bolnica/Bolnica/Controllers/HomeController.cs
+++ Bolnica/Bolnica/Controllers/HomeController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Web;
+using System.Net.Mail;
 using System.Web.Mvc;
+using System.Net;
 
 namespace Bolnica.Controllers
 {
@@ -55,7 +57,17 @@ namespace Bolnica.Controllers
             using (System.IO.StreamWriter file = new System.IO.StreamWriter("E:\\res.txt")) {
                 file.WriteLine(submit["email"]);
             }
-            ViewBag.Result = "success";
+            try
+            {
+                SendEmail(submit["text"], submit["email"], "matvei.nazaruk@gmail.com", "callback", submit["fio"]);
+
+                ViewBag.Result = "success";
+            }
+            catch
+            {
+                ViewBag.Result = "failed";
+
+            }
             return View(); 
         }
 
@@ -63,6 +75,32 @@ namespace Bolnica.Controllers
 
         public ActionResult CallBack() {
             return View();
+        }
+
+        public void SendEmail(string msgBody, string sendTo, string sendFrom, string subject, string toName)
+        {
+            var fromAddress = new MailAddress(sendFrom, toName);
+            var toAddress = new MailAddress(sendFrom, "Sviclochskaya central'naya gorodskaya bolnica");
+            const string fromPassword = "6092726moiNOMER*";
+            string body = msgBody + " My mail: " + sendTo;
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword),
+                Timeout = 20000
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
         }
     }
 }
