5ee57d93bd67a3e936475d26b9ef9d9ae5383b03
try54er <ikrivulets@gmail.com>
2014-01-04 12:58:56 +0300

    first

---
+++ Bolnica/Bolnica/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bolnica.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            ViewBag.Message = "Your service page.";
+
+            return View();
+        }
+
+        public ActionResult Schedule()
+        {
+            ViewBag.Message = "Your service page.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Doctors() {
+            return View();
+        }
+    }
+}
